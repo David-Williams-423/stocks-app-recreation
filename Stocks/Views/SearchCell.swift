367dfdd028be9012a0142ca7d016c04718531b59
David Williams <davidlwilliams423@gmail.com>
2023-10-04 14:01:52 -0400

    search view added

---
+++ Stocks/Views/SearchCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCell.swift
+//  Stocks
+//
+//  Created by David Williams on 10/4/23.
+//
+
+import SwiftUI
+
+struct SearchCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchCell()
+}
